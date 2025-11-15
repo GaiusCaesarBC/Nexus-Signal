f8afb534812fe9107bee72a1db2d1594c4f2437a
Cody Watkins <2codywatkins@gmail.com>
2025-11-15 16:41:57 -0500

    DEBUG: Log process.env.PORT in index.js for Render

--- server/index.js
+++ server/index.js
@@ -2,7 +2,9 @@ require('dotenv').config(); // Load environment variables
 
 const app = require('./app'); // Import the Express app from app.js
 
+console.log(`[DEBUG] process.env.PORT from Render env vars: ${process.env.PORT}`);
 const PORT = process.env.PORT || 5000;
+console.log(`[DEBUG] Final PORT decided by application: ${PORT}`);
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
